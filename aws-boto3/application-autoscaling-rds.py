2eb9d047ee4cef3a7e60c6ec81de3be2b2668acc
RB <rajbhowmick892@gmail.com>
2021-09-07 00:12:50 +0530

    Added Boto3 script for RDS scaling.

---
+++ aws-boto3/application-autoscaling-rds.py
@@ -0,0 +1,50 @@
+"""
+@AUTHOR: Raj Tilak Bhowmick
+This Boto3 script registers a RDS cluster or instance as scalable target and then applies Target-Tracking-Scaling-Policy.
+This can be modified to support other supported services.
+"""
+
+import boto3
+
+
+def registerScalableTarget():
+    try:
+        session = boto3.session.Session(profile_name='YOUR-NAMED-AWS-PROFILE', region_name='TARGET-REGION')
+
+        autoscaling_client = session.client('application-autoscaling')
+
+        response = autoscaling_client.register_scalable_target(
+            ServiceNamespace='rds',
+            ResourceId='cluster:your-db-cluser-name',
+            ScalableDimension='rds:cluster:ReadReplicaCount',
+            MinCapacity=0,
+            MaxCapacity=3,
+        )
+    except Exception as e:
+        print("Exception Raised :: registerScalableTarget() ::" + str(e))
+
+
+def putScalingPolicy():
+    try:
+        session = boto3.session.Session(profile_name='YOUR-NAMED-AWS-PROFILE', region_name='TARGET-REGION')
+
+        autoscaling_client = session.client('application-autoscaling')
+
+        response = autoscaling_client.register_scalable_target(
+            PolicyName='YOUR-POLICY-NAME',
+            ServiceNamespace='rds',
+            ResourceId='',
+            ScalableDimension='rds:cluster:ReadReplicaCount',
+            PolicyType='TargetTrackingScaling',
+            TargetTrackingScalingPolicyConfiguration={
+                'TargetValue': 70.0,
+                'PredefinedMetricSpecification': {
+                    'PredefinedMetricType': 'RDSReaderAverageCPUUtilization',
+                },
+                'ScaleOutCooldown': 600,
+                'ScaleInCooldown': 300,
+                'DisableScaleIn': False
+            }
+        )
+    except Exception as e:
+        print("Exception Raised :: putScalingPolicy() ::" + str(e))
