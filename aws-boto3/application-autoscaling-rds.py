89ad269f6b264a82d773b316e9635057284a8f3d
RB <rajbhowmick892@gmail.com>
2021-09-07 01:11:51 +0530

    Added Boto3 script for RDS scaling.

--- aws-boto3/application-autoscaling-rds.py
+++ aws-boto3/application-autoscaling-rds.py
@@ -1,21 +1,32 @@
 """
 @AUTHOR: Raj Tilak Bhowmick
+@DATE: 07-09-2021
 This Boto3 script registers a RDS cluster or instance as scalable target and then applies Target-Tracking-Scaling-Policy.
 This can be modified to support other supported services.
 """
-
 import boto3
+
+
+def execute():
+    try:
+        session = boto3.session.Session(profile_name='YOUR-NAMED-PROFILE ', region_name='YOUR-TARGET-REGION')
+
+        registerScalableTarget()
+        putScalingPolicy()
+
+    except Exception as e:
+        print("Exception Raised :: execute() ::" + str(e))
 
 
 def registerScalableTarget():
     try:
-        session = boto3.session.Session(profile_name='YOUR-NAMED-AWS-PROFILE', region_name='TARGET-REGION')
+        session = boto3.session.Session(profile_name='YOUR-NAMED-PROFILE', region_name='YOUR-TARGET-REGION')
 
         autoscaling_client = session.client('application-autoscaling')
 
         response = autoscaling_client.register_scalable_target(
             ServiceNamespace='rds',
-            ResourceId='cluster:your-db-cluser-name',
+            ResourceId='cluster:YOUR-CLUSTER-NAME',
             ScalableDimension='rds:cluster:ReadReplicaCount',
             MinCapacity=0,
             MaxCapacity=3,
@@ -33,13 +44,13 @@ def putScalingPolicy():
         response = autoscaling_client.register_scalable_target(
             PolicyName='YOUR-POLICY-NAME',
             ServiceNamespace='rds',
-            ResourceId='',
+            # ResourceId='',
             ScalableDimension='rds:cluster:ReadReplicaCount',
             PolicyType='TargetTrackingScaling',
             TargetTrackingScalingPolicyConfiguration={
                 'TargetValue': 70.0,
                 'PredefinedMetricSpecification': {
-                    'PredefinedMetricType': 'RDSReaderAverageCPUUtilization',
+                    'PredefinedMetricType': 'RDSReaderAverageCPUUtilization',  # RDSReaderAverageDatabaseConnections
                 },
                 'ScaleOutCooldown': 600,
                 'ScaleInCooldown': 300,
@@ -48,3 +59,6 @@ def putScalingPolicy():
         )
     except Exception as e:
         print("Exception Raised :: putScalingPolicy() ::" + str(e))
+
+
+execute()
